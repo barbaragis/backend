a3272009745370e8670084ebf5d53606b86d0ca9
Barbara Gissi <gissibarbara@gmail.com>
2023-10-08 10:49:23 -0300

    Segundo desafio Backend

--- ejercicio.js
+++ ejercicio.js
@@ -1,82 +1,92 @@
+import { promises as fs } from "fs"
+
 class ProductManager {
 
     constructor() {
+        this.path = "./products.txt";
         this.products = []
     }
 
-    getId = () => {
-        const count = this.products.length
-        if (count === 0) return 1
+    static id = 0
 
-        const lastProduct = this.products[count-1]
+    
+    
+    addProduct = async (title , description , price , thumbnail , code, stock) => {
 
-        return lastProduct.id + 1
-    }
+        ProductManager.id++
 
-
-
-    addProduct (title , description , price , thumbnail , code, stock){
-
-    if (!title || !description || !price || !thumbnail || !code || stock === undefined){
-        console.log("Todos los campos son requeridos");
-        return;
-    }
-
-            if(this.products.some(product => product.code === code)){
-                console.log(`El codigo ${code} esta repetido`);
-                return;
-            }
-        
-        
-        const id = this.getId()
-
-
-        this.products.push({
-            id,
-            title, 
-            description, 
-            price ,
-            thumbnail ,
-            code ,
+        let newProduct = {
+            id : ProductManager.id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
             stock
-        })
+        }
 
+        this.products.push(newProduct)
+
+
+        await fs.writeFile(this.path, JSON.stringify(this.products) )
     }
 
-    getProduct (){
-        return this.products
+    readProducts = async () =>{
+        let res = await fs.readFile(this.path , "utf-8")
+        return JSON.parse(res)
     }
 
-    // Producto id
-    exist (id) {
-        return this.products.find((producto) => producto.id === id);
+    getProducts = async () =>{
+        let respuesta = await this.readProducts()
+        return console.log(respuesta)
     }
 
-    getProductById(id) {
-        !this.exist(id) ? console.log ("not found") : console.log (this.exist(id));
+    getProductsById = async (id) => {
+        let respuesta1 = await this.readProducts()
+        let message = typeof respuesta1 === "undefined" ? "Producto inexistente": respuesta1.find((product) => product && product.id === id) || "Producto no encontrado";
+        console.log(message)
     }
 
+    deleteProductosById = async (id) => {
+        let respuesta2 = await this.readProducts();
+        let filter = respuesta2.filter(products=> products.id != id)
+
+
+        await fs.writeFile(this.path, JSON.stringify(filter) )
+        console.log("producto eliminado")
+    }
+
+    updateProducts = async ({id, ...producto}) => {
+        await this.deleteProductosById(id);
+        let oldProduct = await this.readProducts()
+
+        let modify = [{id, ...producto}, ...oldProduct]
+        await fs.writeFile(this.path, JSON.stringify(modify) )
+    }
 }
 
 
 
-const productos = new ProductManager()
-console.log((productos.getProduct()))
-
-//Productos
-productos.addProduct('Producto 1' , 'desc' , 500 , "imagen.jpg" , "123" , 10)
-productos.addProduct('Producto 2' , 'desc' , 700 , "imagen.jpg" , "111" , 15)
-productos.addProduct('Producto 3' , 'desc' , 800 , "imagen.jpg" , "125" , 25 )
+const productos = new ProductManager();
 
 
-// Productos agregados
-console.log((productos.getProduct()))
+/* productos.deleteProductosById(2); */
 
+ 
+/* productos.addProduct('Producto 1' , 'description 1' , 500 , "imagen1.jpg" , "123" , 10)
+productos.addProduct('Producto 2' , 'description 2' , 555 , "imagen2.jpg" , "124" , 9)
+productos.addProduct('Producto 3' , 'description 3' , 400 , "imagen3.jpg" , "111" , 2)  */
 
-//Code repetido 
-productos.addProduct('Producto 4' , 'desc' , 800 , "imagen.jpg" , "111", 4 )
+/* //llama por id 
+productos.getProductsById(2) */
 
-
- //Busca producto por su ID
-productos.getProductById(2); 
-
+//actualizar datos de producto
+productos.updateProducts({
+        id: 1,
+        title: 'Producto 1',
+        description: 'description 1',
+        price: 7000,
+        thumbnail: 'imagen1.jpg',
+        code: '123',
+        stock: 10
+}) 
